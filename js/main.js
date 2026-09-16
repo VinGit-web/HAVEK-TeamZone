65a67a8e8efb070ecb984d7f50d27e1326ba59c9
VinGit-web <vincentknight87@gmail.com>
2026-09-16 11:06:22 +1000

    added js for view event button

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* 
+MAIN JS FILE
+*/
+
+// EVENTS JS
+
+// View event details functionality
+const viewEventButton = document.querySelector(".view-event");
+const eventDetails = document.querySelector(".event-details");
+
+viewEventButton.addEventListener("click", function () {
+    eventDetails.hidden = false;
+});
